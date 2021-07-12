64499abbddb031ceffb7e1cd8f0e7243da01761a
Sungkeun Kim <ksungkeun84@tamu.edu>
2021-07-12 15:53:16 -0400

    updat script

--- celery-run.sh
+++ celery-run.sh
@@ -8,4 +8,5 @@ celery -A gem5art.tasks.celery worker &
 sleep 15
 celery flower -A gem5art.tasks.celery --port=5555 &
 
+cp -r /data/sungkeun/gem5-baseline/gem5/build/X86/gem5.opt /data/sungkeun/gem5-baseline-demo/gem5/build/X86/gem5.opt
 
